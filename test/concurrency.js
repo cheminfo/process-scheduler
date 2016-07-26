b6c956c61d9018c42af4ce8cd2581696ef8fbba5
Daniel Kostro <kostro.d@gmail.com>
2016-07-26 10:00:02 +0200

    add test

--- test/concurrency.js
+++ test/concurrency.js
@@ -143,4 +143,31 @@ describe('success', () => {
         return helper.testSchedule({config, schedule, expect});
     });
 
+    it('same task', function () {
+        var config = {
+            threads: 2
+        };
+
+        var schedule = [
+            {
+                id: 'p1',
+                worker: path.join(__dirname, 'workers/success.js'),
+            },
+            {
+                id: 'p1',
+                worker: path.join(__dirname, 'workers/success.js')
+            }
+        ];
+
+        var expect = {
+            change: [
+                {status: 'queued', id: 'p1'},
+                {status: 'running', id: 'p1'},
+                {status: 'success', id: 'p1'}
+            ]
+        };
+
+        return helper.testSchedule({config, schedule, expect});
+    });
+
 });
